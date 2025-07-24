6b50243246f72c8f423fb3865f6c3f824b4ec21c
ShAYaN <shayanbukhari12345@gmail.com>
2025-07-24 17:04:54 +0500

    eslint configured

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+    /* config options here */
+};
+
+export default nextConfig;
+
+// import type { NextConfig } from 'next';
+// import path from 'path';
+
+// const nextConfig: NextConfig = {
+//     webpack: (config) => {
+//         config.resolve = {
+//             ...config.resolve,
+//             alias: {
+//                 ...(config.resolve?.alias || {}),
+//                 '@': path.resolve(__dirname, 'src'),
+//             },
+//         };
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
